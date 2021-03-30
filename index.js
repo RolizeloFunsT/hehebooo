b29d5f9ec79886c55136fd49afd55f0ba77a83b3
Glitch (fluff-obvious-nigella) <none>
2021-03-30 07:52:02 +0000

    🏇🚝 Checkpoint ./komutlar/çekiliş.js:29775358/15097 ./index.js:29775358/10082

--- index.js
+++ index.js
@@ -265,87 +265,6 @@ client.on("message", async msg => {
 //-------------------- Otorol Sistemi --------------------//
 //-------------------- Otorol Sistemi --------------------//
 
-client.on("guildMemberAdd", member => {
-  var üye = qdb.fetch(`otoroluye_${member.guild.id}`)
-  var bot = qdb.fetch(`otorolbot_${member.guild.id}`)
-  var kanal = qdb.fetch(`otorolkanali_${member.guild.id}`)
-  var embed = qdb.fetch(`otorolmesajtur_${member.guild.id}`)
-  var mesaj = qdb.fetch(`otorolmesaji_${member.guild.id}`)
-  var resim = qdb.fetch(`otorolresim_${member.guild.id}`)
-  var botmesaj = qdb.fetch(`otorolmesajbot_${member.guild.id}`)
-  var ovnırbey = member.guild.owner.id
-  var ovnırcık = client.users.cache.get(ovnırbey)
- var mesajcık = mesaj
-  .replace("{guild}", `**${member.guild}**`)
-  .replace("{guild}", `**${member.guild}**`)
-  .replace("{user}", `**${member}**`)
-  .replace("{user}", `**${member}**`)
-  if(member.user.bot){
-    var botcuk = member.guild.roles.cache.get(bot)
-    if(!botcuk) return;
-   var fadeaway = mesajcık.replace("{role}", `${botcuk}`).replace("{role}", `${botcuk}`)
-    member.roles.add(botcuk.id)
-    var kanalcık = member.guild.channels.cache.get(kanal)
-    if(!kanalcık) return;
-    if(!mesajcık) return ovnırcık.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`Dostum, görünüşe göre ${member.guild.name} isimli sunucunda \`otorol rolü\` ve \`otorol kanalı\` ayarlanmış fakat otorol mesajı ayarlanmamış 😕 Otorol mesajını ayarlamadığın için işlemi durdurdum. Discord boş mesaj göndermeme izin vermiyor 😀 Eğer ayarlamak istiyorsan \`otorol yardım mesaj\` komutunu kullanabilir, neyi ayarlayıp ayarlamadığını görmek istiyorsan \`otorol bilgi\` Komutunu kullanabilirsin. İyi Günler! 🥰`).setFooter("🤖 Bunu bir bot'a rol verirken farkettim!"))
-    if(embed === "embed"){
-    if(resim){
-      const embed = new Discord.MessageEmbed()
-      .setColor('BLACK')
-      .setTitle(member.guild)
-      .setDescription(fadeaway)
-      .setImage(resim)
-      .setFooter(`${member.user.username} geldi!`)
-      .setTimestamp()
-      kanalcık.send(embed)
-      };
-    if(!resim){
-     const embed = new Discord.MessageEmbed()
-      .setColor('BLACK')
-      .setTitle(member.guild)
-      .setDescription(fadeaway)
-      .setFooter(`${member.user.username} geldi!`)
-     .setTimestamp()
-      kanalcık.send(embed)
-    };
-    };
-    if(embed !== "embed"){
-    kanalcık.send(fadeaway)
-    };
-    } else {
-   var rolcük = member.guild.roles.cache.get(üye)
-    if(!rolcük) return;
-    member.roles.add(rolcük.id)
-    var fadeaway = mesajcık.replace("{role}", `${rolcük}`).replace("{role}", `${rolcük}`)
-   var kanalcık = member.guild.channels.cache.get(kanal)
-    if(!kanalcık) return;
-    if(!mesajcık) return ovnırcık.send(new Discord.MessageEmbed().setColor('BLACK').setDescription(`Dostum, görünüşe göre ${member.guild.name} isimli sunucunda \`otorol rolü\` ve \`otorol kanalı\` ayarlanmış fakat otorol mesajı ayarlanmamış 😕 Otorol mesajını ayarlamadığın için işlemi durdurdum. Discord boş mesaj göndermeme izin vermiyor 😀 Eğer ayarlamak istiyorsan \`otorol yardım mesaj\` komutunu kullanabilir, neyi ayarlayıp ayarlamadığını görmek istiyorsan \`otorol bilgi\` Komutunu kullanabilirsin. İyi Günler! 🥰`).setFooter("😎 Bunu bir insana rol verirken farkettim!"))
-    if(embed === "embed"){
-      if(resim){
-      const embed = new Discord.MessageEmbed()
-      .setColor('BLACK')
-      .setTitle(member.guild)
-      .setDescription(fadeaway)
-      .setImage(resim)
-      .setFooter(`${member.user.username} geldi!`)
-      .setTimestamp()
-      kanalcık.send(embed)
-      };
-    if(!resim){
-      const embed = new Discord.MessageEmbed()
-      .setColor('BLACK')
-      .setTitle(member.guild)
-      .setDescription(fadeaway)
-      .setFooter(`${member.user.username} geldi!`)
-      .setTimestamp()
-      kanalcık.send(embed)
-    };
-    };
-    if(embed !== "embed"){
-      kanalcık.send(fadeaway)
-    };
-  }
-})
 
 //-------------------- Otorol Sistemi --------------------//
 //-------------------- Otorol Sistemi --------------------//
@@ -890,3 +809,183 @@ client.on("userUpdate", async (oldUser, newUser) => {
 
   }
 })
+//çekiliş
+
+const data = require('quick.db')
+
+exports.run = async (client, message, args) => {// chimp#0110
+  
+  function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+    function destructMS(milli) {
+        if (isNaN(milli) || milli < 0) {
+          return null;
+        }
+      
+        var d, h, m, s;
+        s = Math.floor(milli / 1000);
+        m = Math.floor(s / 60);
+        s = s % 60;
+        h = Math.floor(m / 60);
+        m = m % 60;
+        d = Math.floor(h / 24);
+        h = h % 24;
+        var yazi;
+        if (d !== 0) yazi = `${d} gün`;
+        if (h !== 0 && yazi) yazi = yazi + `, ${h} saat`;
+        if (h !== 0 && !yazi) yazi = `${h} saat`;
+        if (m !== 0 && yazi) yazi = yazi + `, ${m} dakika`;
+        if (m !== 0 && !yazi) yazi = `${m} dakika`;
+        if (s !== 0 && yazi) yazi = yazi + `, ${s} saniye`;
+        if (s !== 0 && !yazi) yazi = `${s} saniye`;
+        if (yazi) return yazi;
+        if (!yazi) return `1 saniye`;
+      };
+  
+  let ödül = []
+  let kanal = []
+  let zaman = []
+  
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`Gerekli yetkiye sahip değilsin.`)
+const asd = await data.fetch(`..başladı.${message.guild.id}`);
+if(asd) return message.channel.send('Bu sunucuda aktif bir çekiliş zaten var.');
+if(!args[0]) return message.channel.send(`Bir argüman girmelisin: !çekiliş başlat/tekrar`)
+let arg = ['başlat', 'tekrar']
+if(!arg.includes(args[0])) return message.channel.send(`Sadece !çekiliş başlat/tekrar kullanabilirsin.`)
+if(args[0] === 'başlat') {
+try {
+const filter = m => m.author.id == message.author.id;
+     
+message.channel.send(`Önce bir ödül yazmalısın.`).then(() => {
+  
+message.channel.awaitMessages(filter, { maxMatches: 1, time: 60000, errors: ['time'] }).then(collected => {
+message.channel.send(`Ödül: ${collected.first().content}`).then(() => ödül.push(collected.first().content))
+message.delete()
+
+message.channel.send(`Çekiliş hangi kanalda yapılacak?`).then(() => {
+message.channel.awaitMessages(filter, { maxMatches: 1, time: 60000, errors: ['time'] }).then(collected => {
+let as = collected.first().content.replace('<#', '').replace('>', '')
+let ch = message.guild.channels.get(as)
+if(!ch) return message.channel.send(`Etiketlediğin kanalı bulamadım, işlem iptal edildi.`)
+message.delete()
+message.channel.send(`Kanal: ${ch}`).then(() => kanal.push(ch.id))
+  
+message.channel.send(`Çekiliş süresi ne kadar? (1 dakika/1 saat)`).then(() => {
+message.channel.awaitMessages(filter, { maxMatches: 1, time: 60000, errors: ['time'] }).then(collected => {
+let az = collected.first().content;
+
+message.channel.send(`Süre: ${az}`).then(() => zaman.push(collected.first().content)).then(()=> {
+  const sure = zaman.slice(0).join(' ')
+    const bitecegizamanms = Date.now() + ms(sure.replace(' dakika', 'm').replace(' saat', 'h').replace(' saniye', 's').replace(' gün', 'd'))
+  
+    const embed = new Discord.RichEmbed()
+  .setAuthor(client.user.username, client.user.avatarURL)
+  .setTimestamp()
+  .setFooter(`Çekiliş Sistemi`)
+  .setDescription(`**Ödül**: ${ödül.slice(0).join(' ')}
+
+Başlatan: ${message.author.username}
+Zaman: ${sure}
+
+Katılmak için 🎉 tepkisine tıklayın.`)
+  .setTitle(`Bir çekiliş başladı!`)
+message.guild.channels.get(kanal[0]).send(embed).then(async c => {
+message.delete()
+data.set(`çk.${c.id}`)
+data.set(`ödü.${c.id}`, ödül.slice(0).join(' '))
+data.set(`ma.${c.id}`, message.author.id)
+data.set(`..başladı.${message.guild.id}`, {ödül: ödül, host: message.author.username, host1: message.author.tag, message: c.id, channel: kanal[0], süre: bitecegizamanms})
+c.react('🎉').then(async reaction => {
+const interval = setInterval(async function(){
+const kalanzaman = bitecegizamanms - Date.now()   
+
+if (kalanzaman <= 0) {
+clearInterval(interval)
+const kişiler = reaction.users
+await sleep(50)
+const embed = new Discord.RichEmbed()
+  .setAuthor(client.user.username, client.user.avatarURL)
+  .setTimestamp()
+  .setFooter(`Çekiliş Sistemi`)
+  .setDescription(`**Ödül**: ${ödül.slice(0).join(' ')}
+
+Başlatan: ${message.author.username}`)
+.setTimestamp(bitecegizamanms)
+  .setTitle(`Çekiliş bitti!`)
+c.edit(embed)
+
+let asd = c.reactions.get(`🎉`).users.random()
+message.guild.channels.get(kanal[0]).send(`Tebrikler, ${asd}! Bizden ${ödül[0]} kazandın.
+Ödülünü alabilmek için: ${message.author.tag} kişisine ulaş.`)
+data.delete(`..başladı.${message.guild.id}`)
+} else {
+const kalanzamanyazi = destructMS(kalanzaman)
+embed.setDescription(`**Ödül**: ${ödül.slice(0).join(' ')}
+
+Başlatan: ${message.author.username}
+Kalan zaman: ${kalanzamanyazi}
+
+Katılmak için 🎉 tepkisine tıklayın.`)
+c.edit(embed)
+                }
+}, 5000)
+  
+})
+
+})
+
+
+
+
+
+
+
+
+})
+}).catch(collected => { message.channel.send(`Çekiliş süresi girmediğin için iptal edildi.`); });
+})
+}).catch(collected => { message.channel.send(`Çekiliş kanalı girmediğin için iptal edildi.`); });
+})
+}).catch(collected => { message.channel.send(`Çekiliş ödülü girmediğin için iptal edildi.`); });
+})
+
+  
+} catch(err) { return; }    
+}
+  
+if(args[0] === 'tekrar') {
+let channel = message.mentions.channels.first()
+if(!args[1]) return message.channel.send(`Çekilişin yapıldığı kanalı etiketle.`)
+if(!channel) return message.channel.send(`Etiktlediğin kanalı bulamıyorum.`)
+
+let mesaj = args[2]
+if(!mesaj) return message.channel.send(`Bir mesaj ID'si girmeyi unuttun.`)
+if(isNaN(mesaj)) return message.channel.send(`Bir mesaj ID'si girmelisin.`)
+
+let asd = channel.fetchMessage(mesaj).then(async msg => {
+const ads = await data.fetch(`çk.${msg.id}`)
+const ödü = await data.fetch(`ödü.${msg.id}`)
+const ma = await data.fetch(`ma.${msg.id}`)
+if(!ads) return message.channel.send(`Hala bitmemiş olan veya çekiliş mesajı olmayan bir mesajın ID'sini girdin.`)
+let asdd = msg.reactions.get(`🎉`).users.random()
+let arc = msg.reactions.get(`🎉`);
+if(!arc) return message.channel.send(`Bu mesaja kimse tepki vermemiş.`)
+channel.send(`Tebrikler, ${asdd}! Bizden ${ödü} kazandın.
+Ödülünü alabilmek için: ${client.users.get(ma)} kişisine ulaş.`)
+})}
+  
+  
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'çekiliş'
+};// codare
+//çekiliş son
